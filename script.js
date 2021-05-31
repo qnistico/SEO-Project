9345354e67f68e204e5c84d831c462659882110f
Quinton <qnistico@aol.com>
2021-05-30 22:25:41 -0400

    good

--- script.js
+++ script.js
@@ -4,7 +4,47 @@ $( document ).ready(function() {
 
   let mockup = document.getElementById('mockup');
   
-  window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-    mockup.style.left = value * 500 + 'px';
-  });
+  window.addEventListener("scroll", parallax)
+
+function parallax() {
+
+    function getPosition(item) {
+
+        let top = (item.getBoundingClientRect().top / window.innerHeight / 2 * 100).toFixed(0)
+
+        return top
+    }
+
+    let itemsToParallax = document.getElementsByClassName("parallaxify")
+
+    for (let item of itemsToParallax) {
+
+        if (!item.dataset.speedMultiplier) {
+
+            item.dataset.speedMultiplier = "0.7"
+        }
+        item.style.position = "relative"
+
+        item.style.top = getPosition(item) * item.dataset.speedMultiplier + "px"
+    }
+
+    let imagesToParallax = document.getElementsByClassName("parallaxifyBg")
+
+    for (let item of imagesToParallax) {
+
+        function getPosition(item) {
+
+            let top = (item.getBoundingClientRect().top / window.innerHeight / 2 * 100).toFixed(0)
+
+            return top
+        }
+
+        if (!item.dataset.speedMultiplier) {
+
+            item.dataset.speedMultiplier = "0.5"
+        }
+
+        item.style.backgroundAttachment = "none"
+        item.style.backgroundPositionY = getPosition(item) * item.dataset.speedMultiplier + "%"
+    }
+}
