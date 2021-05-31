e13b3f2cc7b9f8bb9589b21756f171ec9c268b4c
Quinton <qnistico@aol.com>
2021-05-30 20:54:52 -0400

    good

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $( document ).ready(function() {
     new WOW().init();
   });
 
-          	
+  let mockup = document.getElementById('mockup');
+  
+  window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    mockup.style.left = value * 500 + 'px';
+  });
